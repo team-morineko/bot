ad39ba68e48b8accab625ca12356978a65c45859
nekotoro <kitsuneko.info@gmail.com>
2025-10-19 06:44:36 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.environ['DISCORD_TOKEN'])
